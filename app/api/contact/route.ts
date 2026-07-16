45e3a74660f6ad3591d799a2b29e8e9942f59ed3
Rockplayer0651 <mohdyounus0651@gmail.com>
2026-07-16 16:56:46 +0530

    API ADDED

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 import { Resend } from "resend"
 
-const resend = new Resend("re_ZDY3FuZU_MnUnCDi98eN1p4XhrD6cvqQZ")
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(request: Request) {
   try {
